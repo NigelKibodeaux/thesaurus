026589741fb626d7cf5454131365a7f32e3d2bee
Nigel Kibodeaux <nigelmail@gmail.com>
2022-11-04 12:16:46 -0800

    reconnecting to the server works

--- server.mjs
+++ server.mjs
@@ -13,13 +13,14 @@ server.listen((process.env.PORT || 8080), () => {
   console.log("Server is Running on port", process.env.PORT || 8080)
 })
 
-const wss = new WebSocketServer({ server })
+const wss = new WebSocketServer({ server, clientTracking: true })
 
 const players = new Map()
 
 wss.on('connection', function connection(ws, req) {
   const playerId = new URL('https://whatever/' + req.url).searchParams.get('player')
   console.log('connection established')
+  console.log('clients:', wss.clients.size)
   players.set(playerId, {ws})
 
   ws.on('message', function message(data) {
@@ -33,6 +34,8 @@ wss.on('connection', function connection(ws, req) {
     broadcastGuess(playerId, guess, handle)
   })
 })
+
+wss.on('error', console.log)
 
 function broadcastGuess(playerId, guess, handle) {
   players.forEach((player, id) => {
